25553ce39708d0495d85329fd0f65d4a39cf99c9
Alexandre Lefol <alexandre.lefol@etud.univ-angers.fr>
2015-04-09 10:05:03 +0200

    lien telechargement

--- algorithms.php
+++ algorithms.php
@@ -6,16 +6,21 @@ $document->header();
 $document->beginSection("corpPage", "formdiv");
 $path=$_GET['var1'];
 $path.='/instancesNK';
-//print($path.'/'.'instanceNK');
 $tabDir=scandir($path);
-//print_r($tabDir);
+
+//liste des instances avec le lien des téléchargements 
 echo '<ul>';
 for($i=2;$i<count($tabDir);$i++) {
 	echo '<li>';
 		echo '<a href='.'"'.'downloadInstanceNK.php?var1='.$path.'/'.$tabDir[$i].'"'.'>'.$tabDir[$i].'</a>';
-	echo '</li>';
+		echo '<a href='.'"'.'downloadInstanceNK.php?var1='.$path.'/'.$tabDir[$i].'"'.'>';
+		echo '<img src="view/ressources/download.gif" height="20" width="20">'; 
+		echo '</a>';
+		echo '</li>';
 }
 echo '</ul>';
+
+
 
 $document->endSection();
 $document->end();
