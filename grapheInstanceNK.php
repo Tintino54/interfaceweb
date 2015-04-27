868df4aab0bf9d7c1ee2c704d828935f3ed36196
Alexandre Lefol <alexandre.lefol@etud.univ-angers.fr>
2015-04-27 17:18:39 +0200

    ok

--- grapheInstanceNK.php
+++ grapheInstanceNK.php
@@ -1,7 +1,15 @@
 <?php
-		echo '<div id="container" style="width:100%; height:400px;"></div>';
-		echo "<script>
-		alert($);
+require_once("view/Document.php");
+$document = new Document("view/style.css", "utf8");
+$document->addJavascript("view/javascript/jquery.js");
+$document->addJavascript("view/javascript/highcharts.js");
+$document->begin();
+$document->header();
+$document->beginSection("corpPage", "formdiv");
+	$instance=$_GET['var1'];
+	$algos=scandir('problemeNK/traces');
+	echo '<div id="container" style="width:100%; height:400px;"></div>';
+	$script="<script>
 	$(function () { 
     $('#container').highcharts({
         chart: {
@@ -11,38 +19,48 @@
             text: 'score algorithmes'
         },
         xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
+           	text: 'iteration'
         },
         yAxis: {
             title: {
                 text: 'Fruit eaten'
             }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
+        },series: [";
+        //création des courbes pour chaque algo
+     	for ($i=2; $i < count($algos); $i++) { 
+     		$script.='{ name: '.'"'.$algos[$i].'", ';
+            $path='problemeNK/traces/'.$algos[$i].'/'.$instance.'/moyenne_algo_trace';
+            $fichier=fopen($path,"r");
+     		$script.='data: [';
+     		fseek($fichier, 0);
+            //on récupère l'ensemble des points
+     		while($ligne=fgets($fichier)){
+                //ajout des coordonnées du point au graphe
+                $script.='['.strstr($ligne,' ',true).', '.strstr($ligne,'0.').'],';
+     		}
+            $script.=' ]}, ';
+		}
+    	//$script.='{ name: '.'"'.$algos[$i].'", ';
+     	$script.="]
+        	});
     	});
-	});
-
-	var chart1; // globally available
-	$(function() {
-      chart1 = new Highcharts.StockChart({
-         chart: {
-            renderTo: 'container'
-         },
-         rangeSelector: {
-            selected: 1
-         },
-         series: [{
-            name: 'USD to EUR',
-            data: usdtoeur // predefined JavaScript array
-         }]
-      });
-   });
-	</script>";
-
+    	var chart1; // globally available
+    	$(function() {
+          chart1 = new Highcharts.StockChart({
+             chart: {
+                renderTo: 'container'
+             },
+             rangeSelector: {
+                selected: 1
+             },
+             series: [{
+                name: 'USD to EUR',
+                data: usdtoeur // predefined JavaScript array
+             }]
+          });
+       });
+    </script>";
+echo $script;
+$document->endSection();
+$document->end();
 ?>
