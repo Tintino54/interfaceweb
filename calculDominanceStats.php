32476f7c4e66d6617856a33ae35abf17e72ccb85
Alexandre Lefol <alexandre.lefol@etud.univ-angers.fr>
2015-05-15 00:54:20 +0200

    calculDominance sans Json

--- calculDominanceStats.php
+++ calculDominanceStats.php
@@ -1,6 +1,6 @@
 <?php
 /* 
-Compare les valeurs de deux valeurs de deux algorithmes
+Compare les valeurs de deux algorithmes
 si l'algo 1 domine l'algo 2 renvoie 1
 si l'algo 2 domine l'ago 2 renvoie 2
 sinon renvoie 0
@@ -11,12 +11,14 @@ function comparaison($tab1,$tab2){
 	$compteur1=0;
 	$compteur2=0;
 	for($i=0;$i<$taille;$i++){
+		$valeurAlgo1=strstr($tab1[$i],"0.");
+		$valeurAlgo2=strstr($tab2[$i],"0.");
 
-		if($tab1[$i]>$tab2[$i]){
+		if($valeurAlgo1>$valeurAlgo2){
 			$compteur1++;
 		}
 
-		else if($tab1[$i]<$tab2[$i]){
+		else if($valeurAlgo2<$valeurAlgo1){
 			$compteur2++;
 		}
 	}
@@ -27,17 +29,25 @@ function comparaison($tab1,$tab2){
 		return 2;
 	}
 	else{
-		return ;
+		return 0;
 	}
 }
 
 function calculDominance($instance,$algo1,$algo2){
+	$tabResultat["algo1"]=array();
+	$tabResultat["algo2"]=array();
+
+	$indiceAlgo1=0;
+	$indiceAlgo2=0;
+
+	//ouverture des fichiers de traces des algorithmes et stockage dans une 
 	$tracesAlgo1=scandir('problemeNK/traces/'.$algo1.'/'.$instance);
 	$tracesAlgo2=scandir('problemeNK/traces/'.$algo2.'/'.$instance);
 	for($i=2;$i<count($tracesAlgo1);$i++){
 		$linesAlgo1[$i-2]=file('problemeNK/traces/'.$algo1.'/'.$instance.'/'.$tracesAlgo1[$i]);
 		$linesAlgo2[$i-2]=file('problemeNK/traces/'.$algo2.'/'.$instance.'/'.$tracesAlgo2[$i]);
 	}
+ 
 	$taille=count($linesAlgo1);
 	for($i=0;$i<$taille;$i++){
 		$currentValuesAlgo1[$i]=$linesAlgo1[$i][0];
@@ -45,13 +55,14 @@ function calculDominance($instance,$algo1,$algo2){
 	}
 
 	$termine=false;
-	$j=1;
-	$tabResultat=array();
+	$valeurPrecedente=-1;
+	$j=0;
 	while(!$termine){
 		$termine=true;
 		$valueChanged=false;
 		for($i=0;$i<$taille;$i++){
 			$num=strstr($currentValuesAlgo1[$i],' ',true);
+
 			if(isset($linesAlgo1[$i][$num+1])){
 				$termine=false;
 				if($linesAlgo1[$i][$num+1]<=$j){
@@ -59,6 +70,7 @@ function calculDominance($instance,$algo1,$algo2){
 					$currentValuesAlgo1[$i]=($num+1).' '.strstr($linesAlgo1[$i][$num+1],'0.');
 				}
 			}
+
 			$num=strstr($currentValuesAlgo2[$i],' ',true);
 			if(isset($linesAlgo2[$i][$num+1])){
 				$termine=false;
@@ -68,18 +80,62 @@ function calculDominance($instance,$algo1,$algo2){
 				}
 			}
 		}
-		$j++;
+	/*	echo $j." ".comparaison($currentValuesAlgo1,$currentValuesAlgo2)."\r\n";
+		echo "<br />";*/
+
 		if(comparaison($currentValuesAlgo1,$currentValuesAlgo2)==1){
-			//do stuff
+			if($valeurPrecedente==1||$valeurPrecedente==0){
+				$tabResultat["algo1"][$indiceAlgo1]->fin++;
+			}
+			else{
+				$indiceAlgo1++;
+				$tabResultat["algo1"][$indiceAlgo1]->debut=$j;
+				$tabResultat["algo1"][$indiceAlgo1]->fin=$j;
+			}
+			$valeurPrecedente=1;
 		}
+
 		else if(comparaison($currentValuesAlgo1,$currentValuesAlgo2)==2){
-			//do stuff
+			if($valeurPrecedente==2||$valeurPrecedente==0){
+				$tabResultat["algo2"][$indiceAlgo2]->fin++;
+			}
+			else{
+				$indiceAlgo2++;
+				$tabResultat["algo2"][$indiceAlgo2]->debut=$j;
+				$tabResultat["algo2"][$indiceAlgo2]->debut=$j;
+			}
+			$valeurPrecedente=2;
 		}
+
 		else{
-			//do stuff
+			if($valeurPrecedente==-1){
+				$tabResultat["algo2"][$indiceAlgo2]->debut=$j;
+				$tabResultat["algo2"][$indiceAlgo2]->fin=$j;
+				$tabResultat["algo1"][$indiceAlgo1]->debut=$j;
+				$tabResultat["algo1"][$indiceAlgo1]->fin=$j;
+
+			}
+			else if($valeurPrecedente==1){
+				$tabResultat["algo1"][$indiceAlgo1]->fin++;	
+				$indiceAlgo2++;
+				$tabResultat["algo2"][$indiceAlgo2]->debut=$j;
+				$tabResultat["algo2"][$indiceAlgo2]->fin=$j;
+			}
+			else if($valeurPrecedente==2){
+				$tabResultat["algo2"][$indiceAlgo2]->fin++;
+				$indiceAlgo1++;
+				$tabResultat["algo1"][$indiceAlgo1]->debut=$j;
+				$tabResultat["algo1"][$indiceAlgo1]->fin=$j;
+			}
+			else{
+				$tabResultat["algo2"][$indiceAlgo2]->fin++;
+				$tabResultat["algo1"][$indiceAlgo1]->fin++;
+			}
+			$valeurPrecedente=0;
 		}
+		$j++;
 	}
-	echo $j;
+	print_r($tabResultat);
 }
 
 calculDominance("nk_128_8_0","algo1","algo2");
